2d6df4d5d0f78ae0ece40f38e06656aaf2c7d9cb
thekuwayama <thekuwayama@gmail.com>
2025-11-15 02:52:28 +0900

    improvement: using let

--- spec/integer_spec.rb
+++ spec/integer_spec.rb
@@ -2,16 +2,24 @@ require_relative 'spec_helper'
 
 RSpec.describe HPACK::Integer do
   context 'Integer' do
+    let(:mask) do
+      0b00000000
+    end
+
     it 'should encode' do
-      expect(HPACK::Integer.encode(10, 5, 0)).to eq "\x0a".b
-      expect(HPACK::Integer.encode(1337, 5, 0)).to eq "\x1f\x9a\x0a".b
-      expect(HPACK::Integer.encode(42, 8, 0)).to eq "\x2a".b
+      expect(HPACK::Integer.encode(10, 5, mask)).to eq "\x0a".b
+      expect(HPACK::Integer.encode(1337, 5, mask)).to eq "\x1f\x9a\x0a".b
+      expect(HPACK::Integer.encode(42, 8, mask)).to eq "\x2a".b
+    end
+
+    let(:cursor) do
+      0
     end
 
     it 'should decode' do
-      expect(HPACK::Integer.decode("\x0a".b, 5, 0)).to eq [10, 1]
-      expect(HPACK::Integer.decode("\x1f\x9a\x0a".b, 5, 0)).to eq [1337, 3]
-      expect(HPACK::Integer.decode("\x2a".b, 8, 0)).to eq [42, 1]
+      expect(HPACK::Integer.decode("\x0a".b, 5, cursor)).to eq [10, 1]
+      expect(HPACK::Integer.decode("\x1f\x9a\x0a".b, 5, cursor)).to eq [1337, 3]
+      expect(HPACK::Integer.decode("\x2a".b, 8, cursor)).to eq [42, 1]
     end
   end
 end
