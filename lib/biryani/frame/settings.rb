461f9a0d4ca4fa29cd61cbc63b71ecb1d67a6405
thekuwayama <thekuwayama@gmail.com>
2025-11-16 05:14:43 +0900

    add Biryani::Frame::Settings

---
+++ lib/biryani/frame/settings.rb
@@ -0,0 +1,17 @@
+module Biryani
+  module Frame
+    class Settings < BinData::Record
+      endian :big
+      uint24 :payload_length
+      uint8  :f_type, value: -> { 0x04 }
+      bit7   :unused
+      bit1   :ack
+      bit1   :reserved
+      bit31  :stream_id, value: -> { 0x00 }
+      array  :setting, initial_length: -> { payload_length / 6 } do
+        uint16 :id
+        uint32 :value
+      end
+    end
+  end
+end
