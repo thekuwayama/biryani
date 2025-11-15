705ef7bf935b03995d61b565078dbf6279d382b9
thekuwayama <thekuwayama@gmail.com>
2025-11-16 05:23:40 +0900

    add Biryani::Frame::PushPromise

---
+++ lib/biryani/frame/push_promise.rb
@@ -0,0 +1,27 @@
+module Biryani
+  module Frame
+    class PushPromise < BinData::Record
+      endian :big
+      uint24 :payload_length
+      uint8  :f_type, value: -> { 0x05 }
+      bit4   :unused1
+      bit1   :padded
+      bit1   :end_headers
+      bit2   :unused2
+      bit1   :reserved1
+      bit31  :stream_id
+      uint8  :pad_length, onlyif: :padded?
+      bit1   :reserved2
+      bit31  :promised_stream_id
+      string :fragment, read_length: :fragment_length
+      string :padding, read_length: -> { pad_length }
+
+      def fragment_length
+        len = payload_length - 4
+        len -= pad_length + 1 if padded
+
+        len
+      end
+    end
+  end
+end
