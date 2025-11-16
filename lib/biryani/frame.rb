b028e9ff1e79faacc3f701e385ffbce9e6c3ff0e
thekuwayama <thekuwayama@gmail.com>
2025-11-16 13:31:20 +0900

    modify: Biryani::Frame::FrameType -> Biryani::FrameType

--- lib/biryani/frame.rb
+++ lib/biryani/frame.rb
@@ -1,17 +1,15 @@
 module Biryani
-  module Frame
-    module FrameType
-      DATA          = 0x00
-      HEADERS       = 0x01
-      PRIORITY      = 0x02
-      RST_STREAM    = 0x03
-      SETTINGS      = 0x04
-      PUSH_PROMISE  = 0x05
-      PING          = 0x06
-      GOAWAY        = 0x07
-      WINDOW_UPDATE = 0x08
-      CONTINUATION  = 0x09
-    end
+  module FrameType
+    DATA          = 0x00
+    HEADERS       = 0x01
+    PRIORITY      = 0x02
+    RST_STREAM    = 0x03
+    SETTINGS      = 0x04
+    PUSH_PROMISE  = 0x05
+    PING          = 0x06
+    GOAWAY        = 0x07
+    WINDOW_UPDATE = 0x08
+    CONTINUATION  = 0x09
   end
 end
 
