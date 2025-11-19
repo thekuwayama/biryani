9b37372dd5e2189d692fda0937aef2c5cdfb05ad
thekuwayama <thekuwayama@gmail.com>
2025-11-19 23:55:30 +0900

    feat: Biryani::Connection#serve(io)

--- lib/biryani/connection.rb
+++ lib/biryani/connection.rb
@@ -5,9 +5,37 @@ module Biryani
     private_constant :CONNECTION_PREFACE, :CONNECTION_PREFACE_LENGTH
 
     # @param io [IO]
+    def serve(io)
+      self.class.read_http2_magic(io)
+
+      _wport = Writer.new.loop
+      # TODO: write Frame::Settings via wport
+
+      loop do
+        _frame = Frame.read(io)
+
+        # TODO: handling frame & stream.new with wport
+      end
+    end
+
+    # @param io [IO]
     def self.read_http2_magic(io)
       s = io.read(CONNECTION_PREFACE_LENGTH)
       abort 'protocol_error' if s != CONNECTION_PREFACE # TODO: send error
+    end
+
+    class Writer
+      def initialize; end
+
+      # @return [Ractor::Port]
+      def loop
+        Ractor.new do
+          loop do
+            io, f = Ractor.receive
+            io.write(f) # TODO: check window
+          end
+        end
+      end
     end
   end
 end
