8a3d21a540769ab18e25442dc6190c68816be7ae
thekuwayama <thekuwayama@gmail.com>
2025-12-05 01:43:30 +0900

    refactor: add spec/frame/ & mv spec/*_spec.rb -> spec/frame/*_spec.rb

--- spec/frame/rst_stream_spec.rb
+++ spec/frame/rst_stream_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require_relative '../spec_helper'
 
 RSpec.describe Frame::RstStream do
   context do
--- spec/frame/settings_spec.rb
+++ spec/frame/settings_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require_relative '../spec_helper'
 
 RSpec.describe Frame::Settings do
   context do
