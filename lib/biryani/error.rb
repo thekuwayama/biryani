0b70c9c241f4df5f664e4cb9538c52fab4b5fd40
thekuwayama <thekuwayama@gmail.com>
2025-12-15 01:38:52 +0900

    fix: StreamError scope

--- lib/biryani/error.rb
+++ lib/biryani/error.rb
@@ -22,20 +22,20 @@ module Biryani
       def goaway(last_stream_id)
         Frame::Goaway.new(last_stream_id, @code, @debug)
       end
+    end
 
-      class StreamError < Error
-        # @param code [Integer]
-        # @param stream_id [stream_id]
-        def initialize(code, stream_id, msg)
-          @code = code
-          @stream_id = stream_id
-          super(msg)
-        end
+    class StreamError < Error
+      # @param code [Integer]
+      # @param stream_id [stream_id]
+      def initialize(code, stream_id, msg)
+        @code = code
+        @stream_id = stream_id
+        super(msg)
+      end
 
-        # @return [RstStream]
-        def rst_stream
-          Frame::RstStream.new(@stream_id, @code)
-        end
+      # @return [RstStream]
+      def rst_stream
+        Frame::RstStream.new(@stream_id, @code)
       end
     end
   end
