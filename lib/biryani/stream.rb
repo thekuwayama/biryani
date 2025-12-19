d40bffd137d63f67596e17183c9d10ef1bd27c0a
thekuwayama <thekuwayama@gmail.com>
2025-12-20 01:22:32 +0900

    modify: rm unnecessary conditional branches

--- lib/biryani/stream.rb
+++ lib/biryani/stream.rb
@@ -6,10 +6,6 @@ module Biryani
 
     # @param tx [Ractor] port
     # @param err [Ractor] port
-    # rubocop: disable Metrics/AbcSize
-    # rubocop: disable Metrics/BlockLength
-    # rubocop: disable Metrics/CyclomaticComplexity
-    # rubocop: disable Metrics/MethodLength
     def initialize(tx, err)
       @rx = Ractor.new(tx, err) do |tx, err|
         bucket = Bucket.new(fields: [], data: '')
@@ -17,11 +13,7 @@ module Biryani
         loop do
           recv_frame = Ractor.receive
 
-          typ = recv_frame.f_type
-          stream_id = recv_frame.stream_id
-          case typ
-          when FrameType::SETTINGS, FrameType::PING, FrameType::GOAWAY
-            err << ConnectionError.new(ErrorCode::PROTOCOL_ERROR, "invalid frame type #{format('0x%02x', typ)} for stream identifier #{format('0x%08x', stream_id)}")
+          case recv_frame.f_type
           when FrameType::DATA
             bucket.data += recv_frame.data
 
@@ -41,29 +33,16 @@ module Biryani
               tx << Frame::Data.new(true, recv_frame.stream_id, 'Hello, world!', nil)
               break
             end
-          when FrameType::PRIORITY
-            self.class.handle_priority(recv_frame)
-          when FrameType::RST_STREAM
-            err << ConnectionError.new(ErrorCode::PROTOCOL_ERROR, 'internal error')
           when FrameType::PUSH_PROMISE
             # TODO
-          when FrameType::WINDOW_UPDATE
-            err << ConnectionError.new(ErrorCode::PROTOCOL_ERROR, 'internal error')
           when FrameType::CONTINUATION
             # TODO: check recv_frame.end_headers?
             bucket.fields += recv_frame.fields
+          else
+            err << ConnectionError.new(ErrorCode::PROTOCOL_ERROR, 'internal error')
           end
         end
       end
-    end
-    # rubocop: enable Metrics/AbcSize
-    # rubocop: enable Metrics/BlockLength
-    # rubocop: enable Metrics/CyclomaticComplexity
-    # rubocop: enable Metrics/MethodLength
-
-    # @param _priority [Priority]
-    def self.handle_priority(_priority)
-      # https://datatracker.ietf.org/doc/html/rfc9113#section-5.3.2
     end
   end
 end
