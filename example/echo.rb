cf8aae5b650092a939873c6633d6d4da600d2269
thekuwayama <thekuwayama@gmail.com>
2025-12-29 13:49:45 +0900

    add example/echo.rb

---
+++ example/echo.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << "#{__dir__}/../lib"
+
+require 'socket'
+require 'biryani'
+
+port = ARGV[0] || 8888
+socket = TCPServer.new(port)
+
+server = Biryani::Server.new(
+  Ractor.shareable_proc do |req, res|
+    res.status = 200
+
+    res.content = if req.method.upcase == 'POST'
+                    req.content
+                  else
+                    ''
+                  end
+
+    res
+  end
+)
+server.run(socket)
+
+# $ bundle exec ruby example/echo.rb
+# $ curl -v --http2-prior-knowledge http://localhost:8888 -X POST -d hi
