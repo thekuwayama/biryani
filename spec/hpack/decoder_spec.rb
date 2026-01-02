d71cad1e097f3e308e9be846202dd4f58e0d8922
thekuwayama <thekuwayama@gmail.com>
2026-01-03 00:06:43 +0900

    tweak: indent

--- spec/hpack/decoder_spec.rb
+++ spec/hpack/decoder_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe HPACK::Decoder do
     it 'should decode' do
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.3.1
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   8286 8441 0f77 7777 2e65 7861 6d70 6c65
-   2e63 6f6d
+  8286 8441 0f77 7777 2e65 7861 6d70 6c65
+  2e63 6f6d
 HEXDUMP
                            )).to eq [
                              [':method', 'GET'],
@@ -20,7 +20,7 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.3.2
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   8286 84be 5808 6e6f 2d63 6163 6865
+  8286 84be 5808 6e6f 2d63 6163 6865
 HEXDUMP
                            )).to eq [
                              [':method', 'GET'],
@@ -31,8 +31,8 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.3.3
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   8287 85bf 400a 6375 7374 6f6d 2d6b 6579
-   0c63 7573 746f 6d2d 7661 6c75 65
+  8287 85bf 400a 6375 7374 6f6d 2d6b 6579
+  0c63 7573 746f 6d2d 7661 6c75 65
 HEXDUMP
                            )).to eq [
                              [':method', 'GET'],
@@ -46,8 +46,8 @@ HEXDUMP
     it 'should decode' do
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.4.1
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   8286 8441 8cf1 e3c2 e5f2 3a6b a0ab 90f4
-   ff
+  8286 8441 8cf1 e3c2 e5f2 3a6b a0ab 90f4
+  ff
 HEXDUMP
                            )).to eq [
                              [':method', 'GET'],
@@ -57,7 +57,7 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.4.2
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   8286 84be 5886 a8eb 1064 9cbf
+  8286 84be 5886 a8eb 1064 9cbf
 HEXDUMP
                            )).to eq [
                              [':method', 'GET'],
@@ -68,8 +68,8 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.4.3
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   8287 85bf 4088 25a8 49e9 5ba9 7d7f 8925
-   a849 e95b b8e8 b4bf
+  8287 85bf 4088 25a8 49e9 5ba9 7d7f 8925
+  a849 e95b b8e8 b4bf
 HEXDUMP
                            )).to eq [
                              [':method', 'GET'],
@@ -83,11 +83,11 @@ HEXDUMP
     it 'should decode' do
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.5.1
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   4803 3330 3258 0770 7269 7661 7465 611d
-   4d6f 6e2c 2032 3120 4f63 7420 3230 3133
-   2032 303a 3133 3a32 3120 474d 546e 1768
-   7474 7073 3a2f 2f77 7777 2e65 7861 6d70
-   6c65 2e63 6f6d
+  4803 3330 3258 0770 7269 7661 7465 611d
+  4d6f 6e2c 2032 3120 4f63 7420 3230 3133
+  2032 303a 3133 3a32 3120 474d 546e 1768
+  7474 7073 3a2f 2f77 7777 2e65 7861 6d70
+  6c65 2e63 6f6d
 HEXDUMP
                            )).to eq [
                              [':status', '302'],
@@ -97,7 +97,7 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.5.2
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   4803 3330 37c1 c0bf
+  4803 3330 37c1 c0bf
 HEXDUMP
                            )).to eq [
                              [':status', '307'],
@@ -107,13 +107,13 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.5.3
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   88c1 611d 4d6f 6e2c 2032 3120 4f63 7420
-   3230 3133 2032 303a 3133 3a32 3220 474d
-   54c0 5a04 677a 6970 7738 666f 6f3d 4153
-   444a 4b48 514b 425a 584f 5157 454f 5049
-   5541 5851 5745 4f49 553b 206d 6178 2d61
-   6765 3d33 3630 303b 2076 6572 7369 6f6e
-   3d31
+  88c1 611d 4d6f 6e2c 2032 3120 4f63 7420
+  3230 3133 2032 303a 3133 3a32 3220 474d
+  54c0 5a04 677a 6970 7738 666f 6f3d 4153
+  444a 4b48 514b 425a 584f 5157 454f 5049
+  5541 5851 5745 4f49 553b 206d 6178 2d61
+  6765 3d33 3630 303b 2076 6572 7369 6f6e
+  3d31
 HEXDUMP
                            )).to eq [
                              [':status', '200'],
@@ -128,10 +128,10 @@ HEXDUMP
     it 'should decode' do
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.6.1
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   4882 6402 5885 aec3 771a 4b61 96d0 7abe
-   9410 54d4 44a8 2005 9504 0b81 66e0 82a6
-   2d1b ff6e 919d 29ad 1718 63c7 8f0b 97c8
-   e9ae 82ae 43d3
+  4882 6402 5885 aec3 771a 4b61 96d0 7abe
+  9410 54d4 44a8 2005 9504 0b81 66e0 82a6
+  2d1b ff6e 919d 29ad 1718 63c7 8f0b 97c8
+  e9ae 82ae 43d3
 HEXDUMP
                            )).to eq [
                              [':status', '302'],
@@ -141,7 +141,7 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.6.2
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   4883 640e ffc1 c0bf
+  4883 640e ffc1 c0bf
 HEXDUMP
                            )).to eq [
                              [':status', '307'],
@@ -151,11 +151,11 @@ HEXDUMP
                            ]
       # https://datatracker.ietf.org/doc/html/rfc7541#appendix-C.6.3
       expect(decoder.decode([<<HEXDUMP.split.join].pack('H*')
-   88c1 6196 d07a be94 1054 d444 a820 0595
-   040b 8166 e084 a62d 1bff c05a 839b d9ab
-   77ad 94e7 821d d7f2 e6c7 b335 dfdf cd5b
-   3960 d5af 2708 7f36 72c1 ab27 0fb5 291f
-   9587 3160 65c0 03ed 4ee5 b106 3d50 07
+  88c1 6196 d07a be94 1054 d444 a820 0595
+  040b 8166 e084 a62d 1bff c05a 839b d9ab
+  77ad 94e7 821d d7f2 e6c7 b335 dfdf cd5b
+  3960 d5af 2708 7f36 72c1 ab27 0fb5 291f
+  9587 3160 65c0 03ed 4ee5 b106 3d50 07
 HEXDUMP
                            )).to eq [
                              [':status', '200'],
--- spec/hpack/fields_spec.rb
+++ spec/hpack/fields_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe HPACK::Fields do
     end
     it 'should decode' do
       expect(HPACK::Fields.decode([<<HEXDUMP.split.join].pack('H*'),
-   8286 8441 8a08 9d5c 0b81 70dc 79e7
-   9e40 8721 eaa8 a449 8f57 88ea 52d6
-   b0e8 3772 ff
+  8286 8441 8a08 9d5c 0b81 70dc 79e7
+  9e40 8721 eaa8 a449 8f57 88ea 52d6
+  b0e8 3772 ff
 HEXDUMP
                                   dynamic_table)).to eq [[':method', 'GET'], [':scheme', 'http'], [':path', '/'], [':authority', '127.0.0.1:8888'], ['connection', 'keep-alive']]
     end
