4838647dac4542eeb954448f84e370b7a0b08015
thekuwayama <thekuwayama@gmail.com>
2026-01-17 16:35:52 +0900

    ci: add conformance to rake tasks

---
+++ conformance/server_spec.rb
@@ -0,0 +1,31 @@
+require_relative 'spec_helper'
+
+RSpec.describe Server do
+  before do
+    @tcpserver = TCPServer.open(PORT)
+
+    Ractor.new(@tcpserver) do |socket|
+      server = Server.new(
+        Ractor.shareable_proc do |_req, res|
+          res.status = 200
+          res.content = 'Hello, world!'
+        end
+      )
+
+      server.run(socket)
+    end
+  end
+
+  let(:client) do
+    which('h2spec')
+    "h2spec --port #{PORT} --verbose"
+  end
+
+  after do
+    @tcpserver.close
+  end
+
+  it 'should run' do
+    system(client)
+  end
+end
